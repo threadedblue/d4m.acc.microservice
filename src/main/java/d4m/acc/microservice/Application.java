c9b2867420fdb153f3476bd290935b395d7588c8
geoffry.roberts <geoffry.roberts@cormac-corp.com>
2025-02-24 11:19:51 -0500

    Put access dependency on the classpath

--- src/main/java/d4m/acc/microservice/Application.java
+++ src/main/java/d4m/acc/microservice/Application.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"d4m.acc.microservice", "d4m.acc.access"})
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
